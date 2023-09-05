a01227ce492914451c3ed168f61e39a9561cfcf8
Angichaika <124779069+Angichaika@users.noreply.github.com>
2023-09-05 07:55:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstappexercise/FirstFragment.kt
@@ -0,0 +1,46 @@
+package com.example.myfirstappexercise
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.myfirstappexercise.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment) }
+
+        AdapterList(listOf("String 1", "String 2", "String 3", "String 4", "String 5","String 6", "String 7", "String 8", "String 9")).also { binding.recycle1 = it }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
